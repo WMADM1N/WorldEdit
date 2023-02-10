6b1e632520bb0dc1a354e908bd863d9ef663f356
sergei <Ser8989@outlook.com>
2023-02-09 21:29:18 -0500

    revert #3bb0424, fix //fill functionality

--- WorldEdit/WorldEdit.cs
+++ WorldEdit/WorldEdit.cs
@@ -146,7 +146,15 @@ namespace WorldEdit
 			{
 				HelpText = "Drains liquids in the worldedit selection."
 			});
-			TShockAPI.Commands.ChatCommands.Add(new Command("worldedit.utils.fixghosts", FixGhosts, "/fixghosts")
+            TShockAPI.Commands.ChatCommands.Add(new Command("worldedit.region.fill", Fill, "/fill")
+            {
+                HelpText = "Fills the worldedit selection."
+            });
+            TShockAPI.Commands.ChatCommands.Add(new Command("worldedit.region.fillwall", FillWall, "/fillwall", "/fillw")
+            {
+                HelpText = "Fills the worldedit selection."
+            });
+            TShockAPI.Commands.ChatCommands.Add(new Command("worldedit.utils.fixghosts", FixGhosts, "/fixghosts")
 			{
 				HelpText = "Fixes invisible signs, chests and item frames."
 			});
@@ -865,6 +873,87 @@ namespace WorldEdit
 				e.Player.SendErrorMessage("Invalid selection.");
 			else
 				_commandQueue.Add(new Drain(info.X, info.Y, info.X2, info.Y2, e.Player));
+		}
+
+		private void Fill(CommandArgs e)
+		{
+			PlayerInfo info = e.Player.GetPlayerInfo();
+			if (info.X == -1 || info.Y == -1 || info.X2 == -1 || info.Y2 == -1)
+			{
+				e.Player.SendErrorMessage("Invalid selection.");
+				return;
+			}
+			if (e.Parameters.Count == 0)
+			{
+				e.Player.SendErrorMessage("//fill <tile> [=> boolean expr...]");
+				return;
+			}
+
+			var tiles = ID.GetTileID(e.Parameters[0].ToLowerInvariant());
+
+			if (tiles.Count == 0)
+			{
+				e.Player.SendErrorMessage("Invalid tile '{0}'!", e.Parameters[0]);
+				return;
+			}
+			else if (tiles.Count > 1)
+			{
+				e.Player.SendErrorMessage("More than one tile matched!");
+				return;
+			}
+
+			Expression? expression;
+			if (e.Parameters.Count > 1)
+			{
+				if (!Parser.TryParseTree(e.Parameters.Skip(1), out expression))
+				{
+					e.Player.SendErrorMessage("Invalid expression!");
+					return;
+				}
+			}
+			else { Parser.TryParseTree(new string[] { "=>", "!t" }, out expression); }
+
+			_commandQueue.Add(new Replace(info.X, info.Y, info.X2, info.Y2, e.Player, -1, tiles[0], expression!));
+		}
+
+		private void FillWall(CommandArgs e)
+		{
+			PlayerInfo info = e.Player.GetPlayerInfo();
+			if (info.X == -1 || info.Y == -1 || info.X2 == -1 || info.Y2 == -1)
+			{
+				e.Player.SendErrorMessage("Invalid selection.");
+				return;
+			}
+			if (e.Parameters.Count == 0)
+			{
+				e.Player.SendErrorMessage("//fill <tile> [=> boolean expr...]");
+				return;
+			}
+
+			var walls = ID.GetWallID(e.Parameters[0].ToLowerInvariant());
+			if (walls.Count == 0)
+			{
+				e.Player.SendErrorMessage("Invalid wall '{0}'!", e.Parameters[0]);
+				return;
+			}
+			else if (walls.Count > 1)
+			{
+				e.Player.SendErrorMessage("More than one wall matched!");
+				return;
+			}
+
+			Expression? expression;
+			if (e.Parameters.Count > 1)
+			{
+				if (!Parser.TryParseTree(e.Parameters.Skip(1), out expression))
+				{
+					e.Player.SendErrorMessage("Invalid expression!");
+					return;
+				}
+			}
+			else { Parser.TryParseTree(new string[] { "=>", "!w" }, out expression); }
+
+			_commandQueue.Add(new ReplaceWall(info.X, info.Y, info.X2, info.Y2, e.Player, 0, walls[0], expression!));
 		}
 
 		private void FixGhosts(CommandArgs e)
